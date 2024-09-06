79f0a60e6fd2aa6aa044ba4f787ec74b13261fe2
Xing Wang <xingwang1991@gmail.com>
2024-09-06 16:07:36 +0200

    add ld1 (#2)

--- src/ase_quantumespresso/__init__.py
+++ src/ase_quantumespresso/__init__.py
@@ -2,6 +2,7 @@ from .espresso import Espresso, PwTemplate
 from .dos import DosTemplate
 from .projwfc import ProjwfcTemplate
 from .pp import PpTemplate
+from .ld1 import Ld1Template
 
 
-__all__ = ["Espresso", "PwTemplate", "DosTemplate", "ProjwfcTemplate", "PpTemplate"]
+__all__ = ["Espresso", "PwTemplate", "DosTemplate", "ProjwfcTemplate", "PpTemplate", "Ld1Template"]
